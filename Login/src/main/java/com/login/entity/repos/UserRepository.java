a991d9bf971128f2eb37d8be743c006e4c68e091
Kanha <Kanha@DESKTOP-534QFMA>
2019-03-17 11:04:55 +0530

    third commit

--- Login/src/main/java/com/login/entity/repos/UserRepository.java
+++ Login/src/main/java/com/login/entity/repos/UserRepository.java
@@ -3,9 +3,10 @@ package com.login.entity.repos;
 import java.sql.SQLException;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.login.entity.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	//Over loaded method of JpaRepository
 	User findByUserName(String userName)throws SQLException;
--- Login/src/main/java/com/login/entity/service/UserServiceImpl.java
+++ Login/src/main/java/com/login/entity/service/UserServiceImpl.java
@@ -1,15 +1,31 @@
 package com.login.entity.service;
 
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.login.entity.repos.UserRepository;
+
 public class UserServiceImpl implements UserDetailsService {
+	@Autowired
+	private UserRepository userRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
+		try {
+			UserDetails userDetails = (UserDetails) userRepository.findByUserName(username);
+			if (userDetails == null) {
+				throw new UsernameNotFoundException("User Name is not valid..");
+			}
+			return userDetails;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return null;
+		
 	}
 
 }
